9c89cb20b2a39510cd7e6697cc307f1172ad8e2c
JagadishPujari <jagadish.pujari@tarento.com>
2020-01-10 15:44:23 +0530

    Issue #SB-16708 feat: Video as solution for reference type questions(Text_Image)

--- src/app/client/src/app/modules/cbse-program/components/question-creation/question-creation.component.ts
+++ src/app/client/src/app/modules/cbse-program/components/question-creation/question-creation.component.ts
@@ -57,6 +57,10 @@ export class QuestionCreationComponent implements OnInit, AfterViewInit, OnChang
   initialized = false;
   showFormError = false;
   editor: any;
+  selectedSolutionType: string;
+  selectedSolutionTypeIndex:string;
+  solutionTypes:any;
+  showSolutionDropDown= true;
   editorState: any;
   solutionUUID: string;
   myAssets = [];
@@ -107,7 +111,8 @@ export class QuestionCreationComponent implements OnInit, AfterViewInit, OnChang
     this.editorConfig = { 'mode': 'create' };
     this.editorState = {
       question : '',
-      answer: ''
+      answer: '',
+      solution: ''
     };
     this.solutionUUID = UUID.UUID();
     this.manageFormConfiguration();
@@ -116,6 +121,18 @@ export class QuestionCreationComponent implements OnInit, AfterViewInit, OnChang
       this.editorState.answer = this.questionMetaData.data.editorState.answer;
       // tslint:disable-next-line:max-line-length
       this.solutionUUID = this.questionMetaData.data.editorState.solutions ? this.questionMetaData.data.editorState.solutions[0].id : this.solutionUUID;
+      if(this.questionMetaData.data.editorState.solution && this.questionMetaData.data.editorState.solution[0].value)
+      {
+        let editor_state = this.questionMetaData.data.editorState;
+        this.editorState.solution = editor_state.solution[0].value;
+        this.solutionUUID = editor_state.solution[0].id;
+        this.selectedSolutionType = editor_state.solution[0].type;
+        this.selectedSolutionTypeIndex = this.getIndexOfSolution();
+      }else{
+        this.editorState.solution = '';
+        this.selectedSolutionType = "";
+      }
+      this.rejectComment = this.questionMetaData.data.rejectComment ? this.questionMetaData.data.rejectComment : '';
       this.mediaArr = this.questionMetaData.data.media || [];
     }
 
@@ -124,6 +141,14 @@ export class QuestionCreationComponent implements OnInit, AfterViewInit, OnChang
     if (this.role.currentRole === 'REVIEWER') {
       this.isReadOnlyMode = true;
     }
+    this.solutionTypes = [{
+      "type":"html",
+      "value":"Text+Image"
+    },
+    {
+      "type":"video",
+      "value":"Video"
+    }];
   }
 
   setUserName() {
@@ -135,6 +160,19 @@ export class QuestionCreationComponent implements OnInit, AfterViewInit, OnChang
       userName += (' ' + this.userService.userProfile.lastName);
     }
     return userName;
+  }
+  selectSolutionType(data:any){
+    let index = _.findIndex(this.solutionTypes, function(sol:any) { return sol.value == data });
+    this.selectedSolutionType = this.solutionTypes[index].type;
+    this.showSolutionDropDown = false;
+  }
+  getIndexOfSolution(){
+    let index = _.findIndex(this.solutionTypes, function(sol:any) { return sol.value == this.selectedSolutionType });
+    return index;
+  }
+  deleteSolution(){
+    this.showSolutionDropDown = true;
+    this.selectedSolutionType = "";
   }
 
   ngAfterViewInit() {
@@ -161,7 +199,17 @@ export class QuestionCreationComponent implements OnInit, AfterViewInit, OnChang
         this.editorState.answer = this.questionMetaData.data.editorState.answer;
         // tslint:disable-next-line:max-line-length
         this.solutionUUID = this.questionMetaData.data.editorState.solutions ? this.questionMetaData.data.editorState.solutions[0].id : this.solutionUUID;
-        this.rejectComment = this.questionMetaData.data.rejectComment;
+        if(this.questionMetaData.data.editorState.solution && this.questionMetaData.data.editorState.solution[0].value)
+      {
+        let editor_state = this.questionMetaData.data.editorState;
+        this.editorState.solution = editor_state.solution[0].value;
+        this.solutionUUID = editor_state.solution[0].id;
+        this.selectedSolutionType = editor_state.solution[0].type;
+      }else{
+        this.editorState.solution = [];
+        this.selectedSolutionType = "";
+      }
+        this.rejectComment = this.questionMetaData.data.rejectComment ? this.questionMetaData.data.rejectComment : '';
       } else {
         this.questionMetaForm.reset();
       }
@@ -238,7 +286,13 @@ export class QuestionCreationComponent implements OnInit, AfterViewInit, OnChang
                   'category': this.sessionContext.questionType === 'curiosity' ? 'CuriosityQuestion' : this.sessionContext.questionType.toUpperCase(),
                     'editorState': {
                       'question': this.editorState.question,
-                      'answer': this.editorState.answer
+                      'answer': this.editorState.answer,
+                      'solution': [{
+                        'id': this.solutionUUID,
+                        'type': this.selectedSolutionType,
+                        'value': this.editorState.solution
+         
+                      }]
                     },
                     'body': rendererBody,
                     'responseDeclaration': {
@@ -300,6 +354,8 @@ export class QuestionCreationComponent implements OnInit, AfterViewInit, OnChang
       this.editorState.question = event.body;
     } else if (type === 'answer') {
       this.editorState.answer = event.body;
+    }else if (type === 'solution') {
+      this.editorState.solution = event.body;
     }
 
     if (event.mediaobj) {
